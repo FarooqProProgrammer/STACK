e4c22937d3a03614415493767d0daaf78374e621
FarooqProProgrammer <farooqayub646@gmail.com>
2022-06-19 07:28:00 -0700

    update commit

--- stack.java
+++ stack.java
@@ -1,5 +1,55 @@
 public class stack{
+    static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data=data;
+            next=null;
+        }
+    }
+    static class Stack{
+        public static Node head;
+        public static boolean isEmpty(){
+            return head==null;
+        }
+        public static void push(int data) {
+            Node newNode=new Node(data);
+            if(isEmpty()){
+                head=newNode;
+                return;
+            }
+            newNode.next=head;
+            head=newNode;
+        }
+        public static int POP() {
+            if(isEmpty()){
+                System.out.println("Stack is Empty");
+            }
+            int top=head.data;
+            head=head.next;
+            return top;
+        }
+
+        public static int peek() {
+            if(isEmpty()){
+                System.out.println("Stack is Empty");
+            }
+            int top=head.data;
+            return head.data;
+        }
+    }
+
      public static void main(String[] args) {
-        System.out.println("HELLO WORLD")
+        Stack s=new Stack();
+        s.push(2);
+        s.push(4);
+        s.push(5);
+        s.push(6);
+
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.POP();
+        }
     }
  }
